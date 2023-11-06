405d29c217918334c6163c1db75aa3e74455a455
Joe Aguado <aguado.joe@gmail.com>
2023-11-06 15:07:46 -0600

    setup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/joeoverflowcode.github.io",
+  // base: "/joeoverflowcode.github.io",
 })
